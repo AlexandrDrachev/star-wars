7888cf8eb56330f857410559b5b41cfd1a3ad9e3
AlexandrDrachev <alexfront.front@gmail.com>
2020-10-27 14:18:43 +0200

    create custom spinner

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,6 +3,10 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { appStateTestAction } from "./redux/app-actions";
 
+import starWarsIcon from "./png/starwars.png";
+import { ReactComponent as StarWarsLogo } from "../spinner/starwars.svg";
+import Spinner from "../spinner/spinner";
+
 const App = () => {
 
     const dispatch = useDispatch();
@@ -10,9 +14,10 @@ const App = () => {
     return (
         <div>
             <div
-                onClick={() => dispatch(appStateTestAction())}
-                className="w-full container mx-auto flex flex-col justify-center items-center text-white">
-                App
+                className="w-full container mx-auto flex flex-col justify-center items-center text-white text-sm">
+                <div className="w-full">
+                    <Spinner w={`w-40`} h={`h-30`} color={`text-indigo-700`} />
+                </div>
             </div>
         </div>
     );
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { ReactComponent as SpinnerLogo } from "./starwars.svg";
+
+const Spinner = ({ w, h, color }) => {
+    return (
+        <div className="relative w-full h-screen overflow-y-hidden">
+            <div className="absolute top-0 right-0 bottom-0 left-0 flex flex-col justify-center items-center">
+                <div className="w-full h-full flex flex-col justify-center items-center">
+                    <div className={`${w} ${h} ${color} spinner`}>
+                        <SpinnerLogo />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
